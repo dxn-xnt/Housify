c04a036af2e0dbdec6cb181c29e2c030688ad825
Roy Adrian C. Rondina <rondina894@gmail.com>
2025-05-03 01:40:13 +0800

    naa nani migrate

--- app/Http/Controllers/PropertyAmenityController.php
+++ app/Http/Controllers/PropertyAmenityController.php
@@ -21,7 +21,7 @@ class PropertyAmenityController extends Controller
     /**
      * Display the specified resource.
      */
-    public function shows(PropertyAmenity $propertyAmenity)
+    public function show(PropertyAmenity $propertyAmenity)
     {
         // Wala pay views
         return view('PropertyAmenity.show', compact('propertyAmenity'));
