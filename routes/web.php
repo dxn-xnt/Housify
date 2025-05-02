b8aed37d200f732d3dbc59ef63bff1e805eca1d1
Roy Adrian C. Rondina <rondina894@gmail.com>
2025-05-03 00:25:24 +0800

    dara

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
-
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\LogInController;
 
 // Home page with property listings
 Route::get('/', [PropertyController::class, 'index'])->name('home');
@@ -11,8 +11,13 @@ Route::get('/', [PropertyController::class, 'index'])->name('home');
 // Log In Page
 Route::get('/login', [UserController::class, 'showLogin'])->name('login');
 
+//User Log in
+Route::post('/user/login', [LogInController::class, 'login'])->name('user.login');
+
 // Sign Up Page
 Route::get('/signup', [UserController::class, 'showSignUp'])->name('signup');
+
+Route::post('/user/create', [UserController::class, 'createUser'])->name('user.create');
 
 // Property details page
 Route::get('/property/{id}', [PropertyController::class, 'show'])->name('property.show');
@@ -22,11 +27,6 @@ Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.ind
 Route::get('/bookings/{id}', [BookingController::class, 'show'])->name('bookings.show');
 Route::post('/property/{id}/book', [BookingController::class, 'book'])->name('bookings.book');
 Route::post('/bookings/{id}/cancel', [BookingController::class, 'cancel'])->name('bookings.cancel');
-
-// Adding properties
-Route::get('/property/create/step1', [PropertyController::class, 'createProperty'])->name('property.create');
-
-
 
 // Fallback route
 Route::fallback(function () {
